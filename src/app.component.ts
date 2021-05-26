bbd32f5e697527a91efef04bb4c97f81583e9d08
kunickyd <danecek37@gmail.com>
2021-05-26 23:44:32 +0200

    chores: - used .resoveEsModule for SPOI symbols - code moved into AppComponent class

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,197 +1,47 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
-import proj4 from 'proj4';
-
-import Feature from 'ol/Feature';
-import Point from 'ol/geom/Point';
 import VectorLayer from 'ol/layer/Vector';
 import View from 'ol/View';
-import {register} from 'ol/proj/proj4';
-import {Group, Tile} from 'ol/layer';
-import {OSM, TileWMS, XYZ} from 'ol/source';
-import {Vector as VectorSource} from 'ol/source';
-import { transform, transformExtent, get as getProjection } from 'ol/proj';
+import { Tile } from 'ol/layer';
+import { OSM } from 'ol/source';
+import { transform } from 'ol/proj';
 import { Icon, Style, } from 'ol/style';
 
 import hr_mappings from './human-readable-names';
 
-import {HsConfig, HsLanguageService} from 'hslayers-ng';
-import {SparqlJson} from 'hslayers-ng';
+import { HsConfig, HsLanguageService, HsUtilsService, SparqlJson } from 'hslayers-ng';
 
 //proj4.defs('EPSG:5514', '+proj=krovak +lat_0=49.5 +lon_0=24.83333333333333 +alpha=30.28813972222222 +k=0.9999 +x_0=0 +y_0=0 +ellps=bessel +towgs84=542.5,89.2,456.9,5.517,2.275,5.516,6.96 +units=m +no_defs');
 //register(proj4);
 //const sjtskProjection = getProjection('EPSG:5514');
-const defaultLayers = [    
-    new Tile({
-        source: new OSM(),
-        title: 'OpenStreetMap',
-        base: true,
-        visible: true,
-        path: 'Roads',
-    }),
-    new Tile({
-        title: 'OpenCycleMap',
-        visible: false,
-        base: true,
-        source: new OSM({
-            url: 'http://{a-c}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png',
-        }),
-        path: 'Roads',
-    }),
-    new Tile({
-        title: 'MTBMap',
-        visible: false,
-        base: true,
-        source: new OSM({
-            // For some reason XYZ() does not work here
-            url: 'http://tile.mtbmap.cz/mtbmap_tiles/{z}/{x}/{y}.png',
-        }),
-        path: 'Roads',
-    }),
-    new Tile({
-        title: 'OwnTiles',
-        visible: false,
-        base: true,
-        source: new OSM({
-            // For some reason XYZ() does not work here
-            url: 'http://ct37.sdi4apps.eu/map/{z}/{x}/{y}.png',
-        }),
-        path: 'Roads',
-    }),
-
-    //WEATHER
-    new Tile({
-        title: 'OpenWeatherMap cloud cover',
-        source: new OSM({
-            // For some reason XYZ() does not work here
-            url:
-                'http://{a-c}.tile.openweathermap.org/map/clouds/{z}/{x}/{y}.png?appid=13b627424cd072290defed4216e92baa',
-        }),
-        visible: false,
-        opacity: 0.7,
-        path: 'Weather info',
-    }),
-    new Tile({
-        title: 'OpenWeatherMap precipitation',
-        source: new OSM({
-            // For some reason XYZ() does not work here
-            url:
-                'http://{a-c}.tile.openweathermap.org/map/precipitation/{z}/{x}/{y}.png?appid=13b627424cd072290defed4216e92baa',
-        }),
-        visible: false,
-        opacity: 0.7,
-        path: 'Weather info',
-    }),
-    new Tile({
-        title: 'OpenWeatherMap temperature',
-        source: new OSM({
-            // For some reason XYZ() does not work here
-            url:
-                'http://{a-c}.tile.openweathermap.org/map/temp/{z}/{x}/{y}.png?appid=13b627424cd072290defed4216e92baa',
-        }),
-        visible: false,
-        opacity: 0.7,
-        path: 'Weather info',
-    })    
-];
-
-//TODO: instead of .defaulting require()ed symbols, use hsUtilsService.resolveEsModule() fction
-//TODO: refactor all this floating code into component/service methods
-const symbols = {
-    car_service: require('../img/symbolsWaze/car_service.png').default,
-    bank: require('../img/symbols/bank.png').default,
-    atm: require('../img/symbols/atm.png').default,
-    cafe: require('../img/symbols/cafe.png').default, 
-    fast_food: require('../img/symbols/fast_food.png').default,
-    pub: require('../img/symbols/pub.png').default,
-    restaurant: require('../img/symbols/restaurant.png').default,
-    hotel: require('../img/symbols/hotel.png').default,
-    supermarket: require('../img/symbols/supermarket.png').default,
-    information: require('../img/symbols/information.png').default,
-    camp_site: require('../img/symbols/camp_site.png').default,
-};
-
-const styleOSM = function (feature, resolution) {
-    if (
-        typeof feature.get('visible') === 'undefined' ||
-        feature.get('visible') == true
-    ) {
-        let s = feature.get('http://www.sdi4apps.eu/poi/#mainCategory');
-        if (typeof s === 'undefined') {
-            return;
-        }
-        s = s.split('#')[1];
-        return [
-            new Style({
-                image: new Icon({
-                    anchor: [0.5, 1],
-                    src: symbols[s],
-                    size: [30, 35],
-                    crossOrigin: 'anonymous',
-                }),
-            }),
-        ];
-    } else {
-        return [];
-    }
-};
-
-function getSpoiLayers(): any[]  {
-
-  let category: string,
-    layers: any[] = [];
-    
-
-  for (category in hr_mappings.popular_categories) {
-        layers.push(new VectorLayer({
-            title: hr_mappings.popular_categories[category],
-            source: new SparqlJson({
-                geom_attribute: '?geom',
-                url:
-                    'https://www.foodie-cloud.org/sparql?default-graph-uri=&query=' +
-                    encodeURIComponent(
-                        'SELECT ?o ?p ?s FROM <http://www.sdi4apps.eu/poi.rdf> FROM <http://www.sdi4apps.eu/poi_changes.rdf> FROM <http://www.sdi4apps.eu/poi_categories.rdf> WHERE { ?o <http://www.w3.org/1999/02/22-rdf-syntax-ns#type>  <' +
-                        category +
-                        '>. ?o <http://www.opengis.net/ont/geosparql#asWKT> ?geom. FILTER(isBlank(?geom) = false). '
-                    ) +
-                    '<extent>' +
-                    encodeURIComponent('?o ?p ?s } ORDER BY ?o') +
-                    '&should-sponge=&format=application%2Fsparql-results%2Bjson&timeout=0&debug=on',
-                updates_url:
-                    'https://www.foodie-cloud.org/sparql?default-graph-uri=&query=' +
-                    encodeURIComponent(
-                        'SELECT ?o ?date ?attr ?value FROM <http://www.sdi4apps.eu/poi.rdf> FROM <http://www.sdi4apps.eu/poi_categories.rdf> FROM <http://www.sdi4apps.eu/poi_changes.rdf> WHERE { ?o <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <' +
-                        category +
-                        '>. ?o <http://www.opengis.net/ont/geosparql#asWKT> ?geom. FILTER(isBlank(?geom) = false).'
-                    ) +
-                    '<extent>' +
-                    encodeURIComponent(
-                        ' ?o <http://purl.org/dc/elements/1.1/identifier> ?id. ?c <http://www.sdi4apps.eu/poi_changes/poi_id> ?id. ?c <http://purl.org/dc/terms/1.1/created> ?date. ?c <http://www.sdi4apps.eu/poi_changes/attribute_set> ?attr_set. ?attr_set ?attr ?value } ORDER BY ?o ?date ?attr ?value'
-                    ) +
-                    '&should-sponge=&format=application%2Fsparql-results%2Bjson&timeout=0&debug=on',
-                //category_field: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type',
-                category: category,
-                projection: 'EPSG:3857',                
-            }),
-            style: styleOSM,
-            visible: false,
-            path: 'Popular Categories'            
-        }));
-    }
-
-    return layers;
-}
-
-function getLayers(): any[] { 
-  return defaultLayers.concat(getSpoiLayers());
-}
 
 @Component({
   selector: 'app-component',
   templateUrl: './app.component.html',
 })
-export class AppComponent {
-  constructor(public HsConfig: HsConfig, public hsLangService: HsLanguageService) {
+export class AppComponent { 
+
+  private symbols: { [key: string]: any } = {
+    'car_service': this.HsUtilsService.resolveEsModule(require('../img/symbolsWaze/car_service.png')),
+    'bank': this.HsUtilsService.resolveEsModule(require('../img/symbols/bank.png')),
+    'atm': this.HsUtilsService.resolveEsModule(require('../img/symbols/atm.png')),
+    'cafe': this.HsUtilsService.resolveEsModule(require('../img/symbols/cafe.png')),
+    'fast_food': this.HsUtilsService.resolveEsModule(require('../img/symbols/fast_food.png')),
+    'pub': this.HsUtilsService.resolveEsModule(require('../img/symbols/pub.png')),
+    'restaurant': this.HsUtilsService.resolveEsModule(require('../img/symbols/restaurant.png')),
+    'hotel': this.HsUtilsService.resolveEsModule(require('../img/symbols/hotel.png')),
+    'supermarket': this.HsUtilsService.resolveEsModule(require('../img/symbols/supermarket.png')),
+    'information': this.HsUtilsService.resolveEsModule(require('../img/symbols/information.png')),
+    'camp_site': this.HsUtilsService.resolveEsModule(require('../img/symbols/camp_site.png')),
+  }  
+
+  constructor(
+    public HsConfig: HsConfig,
+    public hsLangService: HsLanguageService,
+    public HsUtilsService: HsUtilsService,
+    public sanitizer: DomSanitizer,
+  ) {
 
     this.HsConfig.update({
       useProxy: false,
@@ -201,7 +51,7 @@ export class AppComponent {
         : '/proxy/',
       popUpDisplay: 'click',
       open_lm_after_comp_loaded: true,
-      default_layers: getLayers(),
+      default_layers: this.getDefaultLayers().concat(this.getSpoiLayers()),
       componentsEnabled: {
         sidebar: true,
         toolbar: true,
@@ -239,4 +89,152 @@ export class AppComponent {
 
     this.hsLangService.setLanguage('cs_CZ');
   }
+
+  /* PRIVATE METHODS */
+  
+  private getStyleOSM(category: string): Style | void {
+    let symbolSrc = this.symbols[category];
+    //console.log(`symbolSrc: ${symbolSrc}`)
+    if (symbolSrc === undefined) {
+      console.warn(`No symbol found for category: ${symbolSrc}!`);
+      return;
+    }
+
+    return new Style({
+      image: new Icon({
+        anchor: [0.5, 1],
+        src: symbolSrc,
+        size: [30, 35],
+        crossOrigin: 'anonymous',
+      }),
+    });
+  }
+
+  private getDefaultLayers(): any[] {
+    return [
+      new Tile({
+        source: new OSM(),
+        title: 'OpenStreetMap',
+        base: true,
+        visible: true,
+        path: 'Roads',
+      }),
+      new Tile({
+        title: 'OpenCycleMap',
+        visible: false,
+        base: true,
+        source: new OSM({
+          url: 'http://{a-c}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png',
+        }),
+        path: 'Roads',
+      }),
+      new Tile({
+        title: 'MTBMap',
+        visible: false,
+        base: true,
+        source: new OSM({
+          // For some reason XYZ() does not work here
+          url: 'http://tile.mtbmap.cz/mtbmap_tiles/{z}/{x}/{y}.png',
+        }),
+        path: 'Roads',
+      }),
+      new Tile({
+        title: 'OwnTiles',
+        visible: false,
+        base: true,
+        source: new OSM({
+          // For some reason XYZ() does not work here
+          url: 'http://ct37.sdi4apps.eu/map/{z}/{x}/{y}.png',
+        }),
+        path: 'Roads',
+      }),
+
+      //WEATHER
+      new Tile({
+        title: 'OpenWeatherMap cloud cover',
+        source: new OSM({
+          // For some reason XYZ() does not work here
+          url:
+            'http://{a-c}.tile.openweathermap.org/map/clouds/{z}/{x}/{y}.png?appid=13b627424cd072290defed4216e92baa',
+        }),
+        visible: false,
+        opacity: 0.7,
+        path: 'Weather info',
+      }),
+      new Tile({
+        title: 'OpenWeatherMap precipitation',
+        source: new OSM({
+          // For some reason XYZ() does not work here
+          url:
+            'http://{a-c}.tile.openweathermap.org/map/precipitation/{z}/{x}/{y}.png?appid=13b627424cd072290defed4216e92baa',
+        }),
+        visible: false,
+        opacity: 0.7,
+        path: 'Weather info',
+      }),
+      new Tile({
+        title: 'OpenWeatherMap temperature',
+        source: new OSM({
+          // For some reason XYZ() does not work here
+          url:
+            'http://{a-c}.tile.openweathermap.org/map/temp/{z}/{x}/{y}.png?appid=13b627424cd072290defed4216e92baa',
+        }),
+        visible: false,
+        opacity: 0.7,
+        path: 'Weather info',
+      })
+    ];
+  }
+
+  /**
+   * Loads SPOI layers from human-readable-names.ts file.
+   * 
+   * @returns An array of VectorLayers containig SPOI data reference.
+   * */
+  private getSpoiLayers(): VectorLayer[] {
+
+    let category: string,
+      layers: VectorLayer[] = [];
+
+    for (category in hr_mappings.popular_categories) {
+      layers.push(new VectorLayer({
+        title: hr_mappings.popular_categories[category],
+        source: new SparqlJson({
+          geom_attribute: '?geom',
+          url:
+            'https://www.foodie-cloud.org/sparql?default-graph-uri=&query=' +
+            encodeURIComponent(
+              'SELECT ?o ?p ?s FROM <http://www.sdi4apps.eu/poi.rdf> FROM <http://www.sdi4apps.eu/poi_changes.rdf> FROM <http://www.sdi4apps.eu/poi_categories.rdf> WHERE { ?o <http://www.w3.org/1999/02/22-rdf-syntax-ns#type>  <' +
+              category +
+              '>. ?o <http://www.opengis.net/ont/geosparql#asWKT> ?geom. FILTER(isBlank(?geom) = false). '
+            ) +
+            '<extent>' +
+            encodeURIComponent('?o ?p ?s } ORDER BY ?o') +
+            '&should-sponge=&format=application%2Fsparql-results%2Bjson&timeout=0&debug=on',
+          updates_url:
+            'https://www.foodie-cloud.org/sparql?default-graph-uri=&query=' +
+            encodeURIComponent(
+              'SELECT ?o ?date ?attr ?value FROM <http://www.sdi4apps.eu/poi.rdf> FROM <http://www.sdi4apps.eu/poi_categories.rdf> FROM <http://www.sdi4apps.eu/poi_changes.rdf> WHERE { ?o <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <' +
+              category +
+              '>. ?o <http://www.opengis.net/ont/geosparql#asWKT> ?geom. FILTER(isBlank(?geom) = false).'
+            ) +
+            '<extent>' +
+            encodeURIComponent(
+              ' ?o <http://purl.org/dc/elements/1.1/identifier> ?id. ?c <http://www.sdi4apps.eu/poi_changes/poi_id> ?id. ?c <http://purl.org/dc/terms/1.1/created> ?date. ?c <http://www.sdi4apps.eu/poi_changes/attribute_set> ?attr_set. ?attr_set ?attr ?value } ORDER BY ?o ?date ?attr ?value'
+            ) +
+            '&should-sponge=&format=application%2Fsparql-results%2Bjson&timeout=0&debug=on',
+          //category_field: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type',
+          category: category,
+          projection: 'EPSG:3857',
+        }),
+        style: this.getStyleOSM(category.split("#")[1]),
+        visible: false,
+        path: 'Popular Categories'
+      }));
+    }
+
+    return layers;
+  }
+
+  /* PRIVATE METHODS - END*/
 }
