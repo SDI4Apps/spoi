8b72df571180e00f4be8095b176d477b04e16b9e
jmacura <macurajan@gmail.com>
2021-06-05 01:34:19 +0200

    feat: Prettify the attributes list

--- src/pretty-info-panel/pretty-infopanel-feature.component.ts
+++ src/pretty-info-panel/pretty-infopanel-feature.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input} from '@angular/core';
 
+import hr_mappings from '../data/human-readable-names.json';
+
 @Component({
   selector: 'spoi-pretty-infopanel-feature',
   templateUrl: 'pretty-infopanel-feature.component.html',
@@ -7,4 +9,18 @@ import {Component, Input} from '@angular/core';
 export class SpoiPrettyInfopanelFeatureComponent {
   @Input() feature;
   constructor() {}
+
+  prettyAttrName(name: string): string {
+    if (Object.keys(hr_mappings.properties).includes(name)) {
+      return hr_mappings.properties[name];
+    }
+    return name;
+  }
+
+  prettyAttrValue(value: string): string {
+    if (value.includes('#')) {
+      return value.split('#')[1];
+    }
+    return value;
+  }
 }
