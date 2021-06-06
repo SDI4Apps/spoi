3d6e38fc1286d86b9607758d755b2a038024abbc
jmacura <macurajan@gmail.com>
2021-06-06 02:24:31 +0200

    style: Prettify infopanel

--- src/pretty-info-panel/pretty-infopanel-feature.component.ts
+++ src/pretty-info-panel/pretty-infopanel-feature.component.ts
@@ -1,6 +1,8 @@
 import {Component, Input} from '@angular/core';
 
 import hr_mappings from '../data/human-readable-names.json';
+import spoiConfig from '../../spoi.config.json';
+import {SpoiPrettyInfopanelService} from './pretty-infopanel.service';
 
 @Component({
   selector: 'spoi-pretty-infopanel-feature',
@@ -8,7 +10,16 @@ import hr_mappings from '../data/human-readable-names.json';
 })
 export class SpoiPrettyInfopanelFeatureComponent {
   @Input() feature;
-  constructor() {}
+  primaryAttributes: Array<string>;
+  advancedAttributes: Array<string>;
+  metadataAttributes: Array<string>;
+  showAdvanced = false;
+
+  constructor(public prettyInfopanelService: SpoiPrettyInfopanelService) {
+    this.primaryAttributes = spoiConfig.primaryAttributes;
+    this.advancedAttributes = spoiConfig.advancedAttributes;
+    this.metadataAttributes = spoiConfig.metadataAttributes;
+  }
 
   prettyAttrName(name: string): string {
     if (Object.keys(hr_mappings.properties).includes(name)) {
@@ -17,10 +28,21 @@ export class SpoiPrettyInfopanelFeatureComponent {
     return name;
   }
 
-  prettyAttrValue(value: string): string {
+  prettyAttrValue(attribute: string): string {
+    const value = this.feature.attributes.find(
+      (attr) => attr.name === attribute
+    ).value;
     if (value.includes('#')) {
       return value.split('#')[1];
     }
     return value;
+  }
+
+  hasValue(attribute: string): boolean {
+    return this.feature.attributes.some((attr) => attr.name === attribute);
+  }
+
+  toggleAdvanced(): void {
+    this.showAdvanced = !this.showAdvanced;
   }
 }
